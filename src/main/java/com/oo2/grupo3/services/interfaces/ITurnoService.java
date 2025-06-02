8326a5950aeadefd550f8d64bd40efbcd6f9890d
WindarkR <trotta.agustin@gmail.com>
2025-06-02 12:21:27 -0300

    Add files via upload

---
+++ src/main/java/com/oo2/grupo3/services/interfaces/ITurnoService.java
@@ -0,0 +1,10 @@
+package com.oo2.grupo3.services.interfaces;
+
+import com.oo2.grupo3.models.entities.Turno;
+
+public interface ITurnoService {
+	
+    Turno solicitarTurno(Turno turno);
+    
+
+}
