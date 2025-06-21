61542677f5a1ebbfb31f8359dd19059147eb6004
WindarkR <trotta.agustin@gmail.com>
2025-06-20 22:51:06 -0300

    Add files via upload

--- src/main/java/com/oo2/grupo3/models/dtos/responses/HorarioLaboralResponseDTO.java
+++ src/main/java/com/oo2/grupo3/models/dtos/responses/HorarioLaboralResponseDTO.java
@@ -1,8 +1,8 @@
 package com.oo2.grupo3.models.dtos.responses;
 
 import java.time.LocalTime;
-import java.util.List;
 
+import com.oo2.grupo3.models.entities.HorarioLaboral;
 import com.oo2.grupo3.models.enums.DiaSemana;
 
 import lombok.AllArgsConstructor;
@@ -17,25 +17,16 @@ import lombok.Setter;
 @Getter
 @Builder
 public class HorarioLaboralResponseDTO {
-  
+    
     private Integer idHorarioLaboral;
-    private List<DiaSemana> diasSemana;
+    private DiaSemana diaSemana;
     private LocalTime horaInicio;
     private LocalTime horaFin;
 
-    public List<String> getDiasSemanaEsp() {
-        return diasSemana.stream()
-            .map(dia -> {
-                switch (dia) {
-                    case LUNES: return "LUNES";
-                    case MARTES: return "MARTES";
-                    case MIERCOLES: return "MIÉRCOLES";
-                    case JUEVES: return "JUEVES";
-                    case VIERNES: return "VIERNES";
-                    case SABADO: return "SÁBADO";
-                    case DOMINGO: return "DOMINGO";
-                    default: return dia.toString();
-                }
-            }).toList();
+    public HorarioLaboralResponseDTO(HorarioLaboral hl) {
+        this.idHorarioLaboral = hl.getIdHorarioLaboral();
+        this.diaSemana = hl.getDiaSemana();
+        this.horaInicio = hl.getHoraInicio();
+        this.horaFin = hl.getHoraFin();
     }
 }
