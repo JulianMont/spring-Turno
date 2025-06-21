b6122bc35f3d598ca076495bff7c9422aa82622e
WindarkR <trotta.agustin@gmail.com>
2025-06-20 22:52:21 -0300

    Add files via upload

--- src/main/java/com/oo2/grupo3/repositories/IHorarioLaboralRepository.java
+++ src/main/java/com/oo2/grupo3/repositories/IHorarioLaboralRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.oo2.grupo3.models.entities.Empleado;
 import com.oo2.grupo3.models.entities.HorarioLaboral;
 
 @Repository
 public interface IHorarioLaboralRepository extends JpaRepository<HorarioLaboral, Integer> {
     
-    List<HorarioLaboral> findByEmpleado_IdPersona(Integer empleadoId);
+
+	List<HorarioLaboral> findByEmpleado_IdPersonaOrderByDiaSemanaAscHoraInicioAsc(Integer empleadoId);
+    Empleado findByEmpleado_IdPersona(Integer idEmpleado);
     
 }
