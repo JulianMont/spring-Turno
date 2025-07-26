eb8d4c8ecbd5d925dcf90054d70b2f34c6927e77
WindarkR <trotta.agustin@gmail.com>
2025-07-26 12:34:03 -0300

    Add files via upload

--- src/main/java/com/oo2/grupo3/configurations/security/SecurityConfiguration.java
+++ src/main/java/com/oo2/grupo3/configurations/security/SecurityConfiguration.java
@@ -41,12 +41,20 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(auth -> {
             	// Recursos públicos
             	auth.requestMatchers("/css/**", "/js/**", "/imgs/**", "/vendor/**").permitAll();
-
+            	
+            	// Swagger
+            	auth.requestMatchers(
+            		    "/v3/api-docs/**",
+            		    "/swagger-ui/**",
+            		    "/swagger-ui.html"
+            		).authenticated();
+            	
+            
             	// Páginas públicas
             	auth.requestMatchers("/auth/login", "/auth/loginProcess", "/auth/register", "/auth/registerProcess", "/auth/logout", "/home/index").permitAll();
 
             	// Acceso común a ambos roles
-            	auth.requestMatchers("/especialidades/list", "/servicios/list", "/turnos/**").hasAnyRole("USER", "ADMIN");
+            	auth.requestMatchers("/especialidades/list", "/servicios/list","/empleados/**","/turnos/**").hasAnyRole("USER", "ADMIN");
 
             	// Todo lo demás solo ADMIN
             	auth.anyRequest().hasRole("ADMIN");
